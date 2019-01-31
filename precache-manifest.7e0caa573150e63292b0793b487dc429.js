c690e775ecdd94e824251108f7a3380b4728dfd7
kik4 <kik4.contact@gmail.com>
2019-02-01 00:45:25 +0900

    Updates

---
+++ precache-manifest.7e0caa573150e63292b0793b487dc429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90172379d45a94363c53",
+    "url": "/gakitsubu/static/js/runtime~main.90172379.js"
+  },
+  {
+    "revision": "e239f1b6603171d06af9",
+    "url": "/gakitsubu/static/js/main.e239f1b6.chunk.js"
+  },
+  {
+    "revision": "1b010889b12bcdbb2f3e",
+    "url": "/gakitsubu/static/js/1.1b010889.chunk.js"
+  },
+  {
+    "revision": "e239f1b6603171d06af9",
+    "url": "/gakitsubu/static/css/main.e6ef7e3b.chunk.css"
+  },
+  {
+    "revision": "b02f9cca28a17696cd1082f2f66b3d0a",
+    "url": "/gakitsubu/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gakitsubu/precache-manifest.86f6ea5638b86d5f1097086fd8ab6b7b.js"
+  "/gakitsubu/precache-manifest.7e0caa573150e63292b0793b487dc429.js"
 );
 
 workbox.clientsClaim();
