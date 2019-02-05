5f68dd6368530c4d366b3c01e5e8aeb7fe7afd8a
kik4 <kik4.contact@gmail.com>
2019-02-06 02:14:43 +0900

    time

--- src/Creator.tsx
+++ src/Creator.tsx
@@ -2,7 +2,14 @@ import * as React from 'react'
 
 export default class Creator extends React.Component<
   {},
-  { text: string; image: HTMLImageElement | undefined; width: number; height: number }
+  {
+    text: string
+    image: HTMLImageElement | undefined
+    width: number
+    height: number
+    isTimeVisible: boolean
+    time: string
+  }
 > {
   constructor(props: any) {
     super(props)
@@ -11,10 +18,14 @@ export default class Creator extends React.Component<
       image: undefined,
       width: 1280,
       height: this.culcHeight(1280),
+      isTimeVisible: true,
+      time: '00:03:43',
     }
     this.text_onChange = this.text_onChange.bind(this)
     this.file_onChange = this.file_onChange.bind(this)
     this.dl_onClick = this.dl_onClick.bind(this)
+    this.isTimeVisible_onChange = this.isTimeVisible_onChange.bind(this)
+    this.time_onChange = this.time_onChange.bind(this)
   }
 
   culcHeight(width: number): number {
@@ -27,6 +38,32 @@ export default class Creator extends React.Component<
 
   componentDidUpdate() {
     this.updateCanvas()
+  }
+
+  drawRect(param: { ctx: any; x: number; y: number; width: number; height: number; radius: number; color: any }) {
+    const ctx = param.ctx
+    const x = param.x
+    const y = param.y
+    const width = param.width
+    const height = param.height
+    const radius = param.radius || 0
+    const color = param.color
+
+    ctx.save()
+    ctx.fillStyle = color
+    ctx.beginPath()
+    ctx.moveTo(x + radius, y)
+    ctx.lineTo(x + width - radius, y)
+    ctx.arc(x + width - radius, y + radius, radius, Math.PI * 1.5, 0, false)
+    ctx.lineTo(x + width, y + height - radius)
+    ctx.arc(x + width - radius, y + height - radius, radius, 0, Math.PI * 0.5, false)
+    ctx.lineTo(x + radius, y + height)
+    ctx.arc(x + radius, y + height - radius, radius, Math.PI * 0.5, Math.PI, false)
+    ctx.lineTo(x, y + radius)
+    ctx.arc(x + radius, y + radius, radius, Math.PI, Math.PI * 1.5, false)
+    ctx.closePath()
+    ctx.fill()
+    ctx.restore()
   }
 
   updateCanvas() {
@@ -81,6 +118,31 @@ export default class Creator extends React.Component<
       ctx.fillText(c, x, lineY + yi * charHeight, lineHeight)
       yi++
     })
+
+    if (this.state.isTimeVisible) {
+      let text = ''
+      const match = this.state.time.match(/0?0?:?0?(.*)/)
+      if (match) {
+        text = match[1]
+      }
+
+      const right = (1230 * canvasWidth) / 1280
+      const width = ((40 * text.length + 64) * canvasWidth) / 1280
+      const y = (538 * canvasWidth) / 1280
+      const height = (134 * canvasWidth) / 1280
+      const radius = (18 * canvasWidth) / 1280
+      this.drawRect({ ctx, x: right - width, y, width, height, radius, color: 'rgba(0, 0, 0, 0.8)' })
+
+      const fontSize = (76 * canvasWidth) / 1280
+      ctx.font = `normal ${fontSize}px gothic`
+      ctx.fillStyle = 'white'
+      ctx.textAlign = 'center'
+      ctx.textBaseline = 'middle'
+      if (!text) {
+        text = this.state.time
+      }
+      ctx.fillText(text, right - width / 2, y + height / 2)
+    }
   }
 
   text_onChange(e: any) {
@@ -109,6 +171,14 @@ export default class Creator extends React.Component<
     dlLink.href = data
     dlLink.download = 'gakitsubu.png'
     dlLink.click()
+  }
+
+  isTimeVisible_onChange(e: any) {
+    this.setState({ isTimeVisible: !this.state.isTimeVisible })
+  }
+
+  time_onChange(e: any) {
+    this.setState({ time: e.target.value })
   }
 
   render() {
@@ -147,6 +217,15 @@ export default class Creator extends React.Component<
               </span>
             </label>
           </div>
+          <div>
+            <label className="checkbox">
+              <input type="checkbox" checked={this.state.isTimeVisible} onChange={this.isTimeVisible_onChange} />
+              動画の長さを表示
+            </label>
+          </div>
+          {this.state.isTimeVisible && (
+            <input className="input" type="time" step="1" value={this.state.time} onChange={this.time_onChange} />
+          )}
           {!this.isIOS && (
             <button className="button" onClick={this.dl_onClick}>
               画像をダウンロード
