001f74140c88279c3d6915a946f700ca7eb130cf
kik4 <kik4.contact@gmail.com>
2019-02-06 23:57:15 +0900

    style

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,20 @@ export default () => {
   return (
     <section className="section">
       <div className="container">
-        <h1 className="title">ガキが舐めてると潰すぞメーカー</h1>
+        <h1 className="title">ガキが舐めてると潰すぞメーカー </h1>
         <Creator />
+        <div className="share">
+          <a
+            href="https://twitter.com/share"
+            className="twitter-share-button"
+            data-show-count="false"
+            data-url="https://kik4.github.io/nametsubu/"
+            data-text="#ガキが舐めてると潰すぞメーカー"
+          >
+            Tweet
+          </a>
+        </div>
+        <span className="version">ver 1.0.0</span>
       </div>
     </section>
   )
